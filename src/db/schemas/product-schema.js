e64174d9962fced3795d3efc7defd9ba0f803954
Juhyeon <gomi.dev1755@gmail.com>
2022-05-25 04:46:13 +0900

    [add] product schema 'description' 추가

--- src/db/schemas/product-schema.js
+++ src/db/schemas/product-schema.js
@@ -30,6 +30,10 @@ const ProductSchema = new Schema(
         type: String,
         required: true,
       },
+      description: {
+        type: String,
+        required: true,
+      },
     },
     {
       collection: 'products',
