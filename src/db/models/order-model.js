b93bd76dd134fb07d09a72f3f94d8e6b157ac3f7
kereu <dnjwm8612@gmail.com>
2022-05-27 01:44:54 +0900

    [fix] 주문 Model 변경 비회원 구매 프로세스 추가로 비회원 주문 조회 method 추가, 불필요한 주문 데이터 삭제 method 제거, 주문 상태(취소, 완료)를 변경하는 주문 상태 변경 method 추가

--- src/db/models/order-model.js
+++ src/db/models/order-model.js
@@ -4,28 +4,34 @@ import {OrderSchema} from '../schemas/order-schema';
 const Order = model('orders', OrderSchema);
 
 export class OrderModel {
-  // 주문 조회(유저)
+  // 주문 조회(회원)
   async findById(userId) {
-    const order = await Order.findOne({user: userId});
+    const order = await Order.findOne({userId: userId});
     return order;
   }
-  // 주문 추가
-  async create(orderInfo) {
-    const createdNewOrder = await Order.create(orderInfo);
-    return createdNewOrder;
+  // 주문 조회(비회원)
+  async findById(fullname, phoneNumber) {
+    const filter = {fullname: fullname, phoneNumber: phoneNumber};
+    const order = await Order.findOne(filter);
+    return order;
   }
   // 주문 조회(관리자)
   async findAll() {
     const orders = await Order.find({});
     return orders;
   }
-  // 주문 삭제
-  async delete(userId) {
-    if (userId) {
-      return await Order.deleteMany({user: userId});
-    } else {
-      return await Order.deleteMany({});
-    }
+  // 주문 추가
+  async create(orderInfo) {
+    const createdNewOrder = await Order.create(orderInfo);
+    return createdNewOrder;
+  }
+  // 주문 상태 변경(취소, 완료)
+  async update(orderId, update) {
+    const filter = {_id: orderId};
+    const option = {returnOriginal: false};
+
+    const updatedOrder = await Order.findByIdAndUpdate(filter, update, option);
+    return updatedOrder;
   }
 }
 
