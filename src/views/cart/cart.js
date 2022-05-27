a3f230fe79a1bf7a4f8ad624e24069a2abc5ded2
minsoo <minsu2344@naver.com>
2022-05-28 02:48:59 +0900

    [fix] 수량변경 하나삭제 완성

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -6,23 +6,26 @@ const deleteBtn = document.querySelector('#deleteBtn');
 const totalPrice = document.querySelector('#orderBtn');
 const productBox = document.querySelector('#product-box');
 
-const deleteOneBtn = document.querySelectorAll('.delete')
 
 const PRODUCTS_KEY = 'products';
 
-let products = [{_id: 'djf20', url: '../tabIcon.png', name: '나이키', size: 'L', price: '19000', quantity: 1}, {_id: '12fd1', url: '../tabIcon.png', name: '나이키', size: 'L', price: '19000', quantity: 1}];
+
+let products = [{_id: "djf20", url: "../tabIcon.png", name: "나이키", size: "L", price: "19000", quantity: 1}, {_id: "12fd1", url: "../tabIcon.png", name: "나이키", size: "L", price: "19000", quantity: 1}];
+
 
 // 로컬스토리지 저장
 function saveProducts() {
   localStorage.setItem(PRODUCTS_KEY, JSON.stringify(products));
 }
 
+
 // 로컬 스토리지 받기
 function getProducts() {
   setProductNum();
+  if(productBox.innerHTML !== '') productBox.innerHTML = '';
 
   const items = JSON.parse(localStorage.getItem(PRODUCTS_KEY));
-  for(let i=0; i<products.length; i++) {
+  for(let i=0; i<JSON.parse(localStorage.products).length; i++) {
     const index = i+1;
     const {url, name, size, price, quantity} = items[i];
     const result = price*quantity;
@@ -37,6 +40,7 @@ function getProducts() {
     );
   }
 }
+
 
 // 화면에 getProducts 띄우기
 function paintProductBox(index, url, name, size, price, quantity, result) {
@@ -53,19 +57,20 @@ function paintProductBox(index, url, name, size, price, quantity, result) {
         </div>
         <p class="product-price">${Number(price).toLocaleString()}</p>
         <div class="product-quantity-box">
-          <input type="button" value="-" onclick="changeValue();">
+          <input type="button" value="-" onclick="changeValue(this);">
           <input type="number" class="product-quantity" value="${quantity}">
-          <input type="button" value="+" onclick="changeValue();">
+          <input type="button" value="+" onclick="changeValue(this);">
         </div>
         <p class="product-result">${Number(result).toLocaleString()}</p>
         <div class="delete-box">
-          <input class="delete" type="button" value="삭제하기">
+          <input class="delete" type="button" value="삭제하기" onClick="deleteOne(this);">
         </div>
       </div>
     </div>
   </div>
   `
 }
+
 
 // 상품 갯수 innerText 정리 함수
 function setProductNum() {
@@ -74,28 +79,15 @@ function setProductNum() {
   totalInProducts.innerText = `전체 ${numberOfProducts}개`;
 }
 
-// 삭제하기 버튼 이벤트 함수
-function deleteOne(e) {
-  const result = confirm('장바구니에서 제거하시겠습니까?');
-
-  if(result) {
-    const product = e.target.parentElement.parentElement;
-    products = products.filter(x => x._id !== product.firstElementChild.innerText) ;
-    product.remove();
-    saveProducts();
-
-    setProductNum();
-  }
-}
 
 // 전체삭제 버튼 클릭 함수
 function handleDeleteAllBtnClick() {
-  if(!localStorage.products) {
+  if(localStorage.products.length === 0) {
     alert('장바구니가 비어있습니다.');
     return;
   }
   const result = confirm('장바구니를 비우시겠습니까?');
-
+  
   if(result) {
     products = [];
     localStorage.removeItem(PRODUCTS_KEY);
@@ -104,21 +96,52 @@ function handleDeleteAllBtnClick() {
 }
 
 
+// 삭제하기 버튼 이벤트 함수
+function deleteOne(target) {
+  const result = confirm('장바구니에서 제거하시겠습니까?');
+
+  if(result) {
+    const product = target.parentElement.parentElement;
+    const index = Number(product.firstElementChild.innerText);
+    // products = products.filter(x => x._id !== product.firstElementChild.innerText) ;
+    products.splice(index-1, 1);
+
+    saveProducts(products);
+    getProducts();
+  }
+}
+
+
+// 수량 변경 함수
+function changeValue(target) {
+  let quant = Number(target.parentElement.firstElementChild.nextElementSibling.value);
+  if(target.value === '+') {
+    quant++;
+  }
+  else {
+    if(quant === 0) {
+      return;
+    }
+    quant--;
+  }
+
+  const index = Number(target.parentElement.parentElement.firstElementChild.innerText)
+  products[index-1].quantity = quant;
+
+  saveProducts(products);
+  getProducts();
+}
 
 
 
-
-
-
-// 각 삭제하기 버튼 클릭 이벤트
-Array.from(deleteOneBtn).forEach(x => x.addEventListener('click', deleteOne));
-
-// 전체삭제 버튼 클릭 이벤트
-deleteAllBtn.addEventListener('click', handleDeleteAllBtnClick);
 
 
 
 
 // 상품 갯수 innerText 정리
-saveProducts(products);
+// saveProducts(products);
 getProducts();
+
+
+// 전체삭제 버튼 클릭 이벤트
+deleteAllBtn.addEventListener('click', handleDeleteAllBtnClick);
