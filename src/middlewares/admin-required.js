c85833fa61affa3230498c4eb1ba00fc3eb7cb02
Juhyeon <gomi.dev1755@gmail.com>
2022-05-30 09:22:12 +0900

    [add] admin-require

---
+++ src/middlewares/admin-required.js
@@ -0,0 +1,13 @@
+function adminRequired(req, res, next) {
+  if (req.currentUserRole !== 'admin') {
+    console.log('관리자 계정으로 로그인해주세요.');
+    res.status(403).json({
+      result: 'forbidden-approach',
+      reason: '관리자만 사용할 수 있는 서비스입니다.',
+    });
+    return;
+  }
+  next();
+}
+
+export {adminRequired};
