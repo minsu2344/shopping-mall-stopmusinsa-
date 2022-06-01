fda41ade8cda08dcb91ee85ba6db4a3fe727fd8e
dohveloper <dohveloper@gmail.com>
2022-06-01 21:03:15 +0900

    [fix] fix router issues

--- src/app.js
+++ src/app.js
@@ -17,6 +17,14 @@ app.use(express.urlencoded({extended: false}));
 // html, css, js 라우팅
 app.use(viewsRouter);
 
+
+// static file 경로 추가
+console.log(__dirname);
+app.use('/assets', express.static(__dirname));
+// static files for components
+app.use('/src', express.static(__dirname));
+app.use('/styles', express.static(__dirname + '/views/styles'));
+
 // api 라우팅
 // 아래처럼 하면, userRouter 에서 '/login' 으로 만든 것이 실제로는 앞에 /api가 붙어서
 // /api/login 으로 요청을 해야 하게 됨. 백엔드용 라우팅을 구분하기 위함임.
